8270766a9dbdf99773777fec23ddc05daa992fb7
mTvare6 <mTvare6@users.noreply.github.com>
2021-08-31 22:31:08 +0530

    remove: random code

--- src/main.cpp
+++ src/main.cpp
@@ -13,27 +13,6 @@ namespace fs = std::filesystem;
 #define KeyColor color::fg::DarkCyan
 #define DiskColor color::fg::LightWhite
 
-std::string repeat(std::string str, const std::size_t n)
-{
-    if (n == 0) {
-        str.clear();
-        str.shrink_to_fit();
-        return str;
-    } else if (n == 1 || str.empty()) {
-        return str;
-    }
-    const auto period = str.size();
-    if (period == 1) {
-        str.append(n - 1, str.front());
-        return str;
-    }
-    str.reserve(period * n);
-    std::size_t m {2};
-    for (; m < n; m *= 2) str += str;
-    str.append(str.c_str(), (n - (m / 2)) * period);
-    return str;
-}
-
 struct HumanReadable {
   std::uintmax_t size{};
   private: friend
